a904dffe2d8b4d18c4623b2c13190f726aa830fb
luckydonald <brony_meetup_calendar_bot+code@luckydonald.de>
2023-06-08 02:57:29 +0200

    Added logging.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from brony_meetup_bot.main import start
+from luckydonaldUtils.logger import logging
 
 
 if __name__ == '__main__':
+    logging.add_colored_handler('root', level=logging.DEBUG)
     start()
 # end def
