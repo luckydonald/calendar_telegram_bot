ebd6be8902db63cc4c703eb5320c6b3f855e5eba
luckydonald <mac2019.git@luckydonald.de>
2023-06-17 16:58:30 +0200

    Updated for more languages.

--- brony_meetup_bot/monospace.py
+++ brony_meetup_bot/monospace.py
@@ -1,20 +1,43 @@
-MONOSPACE_INPUT = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-MONOSPACE_OUTPUT = "𝟶𝟷𝟸𝟹𝟺𝟻𝟼𝟽𝟾𝟿𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉"
-MONOSPACE_REPLACEMENTS = {
-    MONOSPACE_INPUT[i]: MONOSPACE_OUTPUT[i]
-    for i in range(max(len(MONOSPACE_INPUT), len(MONOSPACE_OUTPUT)))
-}
-MONOSPACE_REVERSE_REPLACEMENTS = {
-    MONOSPACE_OUTPUT[i]: MONOSPACE_INPUT[i]
-    for i in range(max(len(MONOSPACE_INPUT), len(MONOSPACE_OUTPUT)))
+from enum import IntEnum, auto as a
+INPUT = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+class Fonts(IntEnum, str):
+    MONOSPACE = a()
+    ITALIC = a()
+    BOLD = a()
+# end class
+
+
+OUTPUT: dict[Fonts, str] = {
+    Fonts.MONOSPACE: "𝟶𝟷𝟸𝟹𝟺𝟻𝟼𝟽𝟾𝟿𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉",
+    Fonts.ITALIC: "𝟢𝟣𝟤𝟥𝟦𝟧𝟨𝟩𝟪𝟫𝘢𝘣𝘤𝘥𝘦𝘧𝘨𝘩𝘪𝘫𝘬𝘭𝘮𝘯𝘰𝘱𝘲𝘳𝘴𝘵𝘶𝘷𝘸𝘹𝘺𝘻𝘈𝘉𝘊𝘋𝘌𝘍𝘎𝘏𝘐𝘑𝘒𝘓𝘔𝘕𝘖𝘗𝘘𝘙𝘚𝘛𝘜𝘝𝘞𝘟𝘠𝘡",
+    Fonts.BOLD: "𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭",
 }
 
 
-def replace_with_monospace(text: str) -> str:
-    return "".join([MONOSPACE_REPLACEMENTS.get(char, char) for char in text])
+REPLACEMENTS: dict[Fonts, dict[str, str]] = {
+    font: {
+        INPUT[i]: replacement[i]
+        for i in range(max(len(INPUT), len(replacement)))
+    }
+    for font, replacement in OUTPUT.items()
+}
+
+REVERSE_REPLACEMENTS: dict[Fonts, dict[str, str]] = {
+    font: {
+        replacement[i]: INPUT[i]
+        for i in range(max(len(INPUT), len(replacement)))
+    }
+    for font, replacement in OUTPUT.items()
+}
+
+
+def replace(text: str, font: Fonts, reverse: bool = False) -> str:
+    replacements = REVERSE_REPLACEMENTS if reverse else REPLACEMENTS
+    return replace_with(text=text, replacements=replacements[font])
 # end def
 
 
-def replace_with_plain(text: str) -> str:
-    return "".join([MONOSPACE_REVERSE_REPLACEMENTS.get(char, char) for char in text])
+def replace_with(text: str, replacements: dict[str, str]) -> str:
+    return "".join([replacements.get(char, char) for char in text])
 # end def
